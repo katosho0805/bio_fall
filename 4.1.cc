ad68bf9132e37ce2e9fca060e4b3de5b180915ee
katosho0805 <ss.kato20030805@keio.jp>
2025-10-17 06:43:26 +0000

    majiwakarannti

---
+++ 4.1.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+using namespace std;
+
+const int cell_num=401;
+const int time_step=200;
+
+void print(const vector<int> & cells,outfile){
+    for(int cell_state)
+
+}
+
+int main (void){
+    int rule_num=0;
+    cin>>rule_num;
+
+
+}
+
