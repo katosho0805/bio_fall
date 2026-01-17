d23b9a5d674a59e2da30311038fb1e95f7bfa372
katosho0805 <ss.kato20030805@keio.jp>
2026-01-17 05:04:22 +0000

    おｋ

--- 11.1.test.cc
+++ 11.1.test.cc
@@ -8,7 +8,9 @@
 
 using namespace std;
 
-void LoadExpressinFile(string file_name,vector<string >& tissue_name,vector<string>& gane_name,){
+
+
+void LoadExpressinFile(string file_name,vector<string >& tissue_name,vector<string>& gane_name){
 
     ifstream ifs(file_name);
     if(!ifs){
@@ -35,5 +37,19 @@ int main(void){
     vector<string> tissue_name(NUM_TISSUES,"");
     vector<string> gene_name(NUM_GENES,"");
     vector<vector<double>>expression_matrix(NUM_GENES,vector<double>(NUM_TISSUES,0.0));
-    LoadExpressionFile("gene_expression.txt",tissue_name,gene_name,expression_mmatrix);
+    LoadExpressionFile("gene_expression.txt",tissue_name,gene_name,expression_matrix);
+
+    vector<pair<double,int>>mean_ranking(NUM_GENES);
+    vector<pair<double,int>>score_ranking(NUM_GENES);
+ 
+    for(int i=0;i<NUM_GENES;i++){
+        double sum=0.0;
+        for(int j=0;j<NUM_TISSUES;j++){
+            sum += expression_matrix[i][j];
+        }
+        doouble mean=sum/NUM_TISSUES;
+
+        mean_ranking[i]=make_pair(mean,i);
+    }
+
 }
