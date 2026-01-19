a98f17ba3fa2222d0738b675427dfc822bbcdec8
katosho0805 <ss.kato20030805@keio.jp>
2026-01-19 08:53:25 +0000

    ok

--- 10.2.test.cc
+++ 10.2.test.cc
@@ -4,20 +4,20 @@ typedef  unsigned char uchar;
 using namespace std;
 
 int decide_num (const image&gray){
-     vector<int> hist(256, 0);
+     vector<int> hist(256, 0);//ヒストグラム
 
      for(int y=0;y<gray.h;y++){
         for(int x=0;x<gray.w;x++){
-            uchar v= gray.getPixel(y,x,0;);
+            uchar v= gray.getPixel(y,x,0);
             hist++;
         }
      }
 
-     double m_t=0.0;
-     int n=gray.w*gray.h;
+     double m_t=0.0;//全体の平均
+     int n=gray.w*gray.h;//全画素数
 
      for(int i=0;i<n;i++){
-        m_t=i*(double)hist[i];
+        m_t=i*(double)hist[i]; //(画素の明るさ)*(その明るさを持つ個数)
      }
      m_t/=n;
 
@@ -27,18 +27,18 @@ int decide_num (const image&gray){
      double sum_b=0;//黒クラスに含まれる画素の 画素値（明るさ）の合計
 
      for(int t=0;t<255;t++){
-        w_b+=hist[t];
-        int w_h=n-w_b;
+        w_b+=hist[t];//hist[t]は明るさがtである画素数
+        int w_h=n-w_b;//w_hは全画素数から黒を引いたもの
 
-        sum_b+=t*(double)hist[t];
+        sum_b+=t*(double)hist[t];//黒の(画素の明るさ)*(その明るさを持つ個数)
 
-        double m_b=sum_b/w_b;
-        double m_h=(m_t*n-sum_b)/w_h;
+        double m_b=sum_b/w_b;//黒クラスの平均画素値
+        double m_h=(m_t*n-sum_b)/w_h;//白クラスの平均画素数
 
-        double sigma_b2=w_b*(m_b-m_t)*(m_b-m_t)+w_h*(m_h-m_t)*(m_h-m_t);
+        double sigma_b2=w_b*(m_b-m_t)*(m_b-m_t)+w_h*(m_h-m_t)*(m_h-m_t);//クラス間分散
 
         if(sigma_b2>max_sigma_b2){
-            max_sigma_b2-sigma_b2;
+            max_sigma_b2=sigma_b2;
             best_t=t;
         }
      }
